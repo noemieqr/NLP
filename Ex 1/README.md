4ed9c1c061491b35ebcf5704619c2c09ffa0ec4e
kdlinda <kocsis.doralinda@gmail.com>
2018-02-09 22:03:13 +0100

    updated Ex1 repository

--- Ex 1/README.md
+++ Ex 1/README.md
@@ -1,8 +1,9 @@
 # Goal
-Implement skip-gram with negative-sampling from scratch. 
+Implement skip-gram with negative-sampling from scratch.
 # Formatting
 - call your main python function (the one that should be executed) main.2.py or main.3.py
 - please use main.2.py if you use python2.x and main.3.py if you use python3.x
-- do not rely on any libraries beyond numpy, scikit-learn, spacy. If you need otheres, please first check with me there should be
+- do not rely on any libraries beyond numpy, scikit-learn, spacy. If you need others, please first check with me there should be
 - one file variable __authors__ which is a list of strings containing your names
-- one file variable __emaisl__ which is a list of strings containing your emails. Please revise this one carefuly keep the arguments format as given in the template (--text, --model, --test)
+- one file variable __emails__ which is a list of strings containing your emails. 
+Please revise this one carefully keep the arguments format as given in the template (--text, --model, --test)
---
+++ Ex 1/main.3.py
@@ -0,0 +1,72 @@
+#import libraries
+from __future__ import division
+import argparse
+import pandas as pd
+
+#to be used
+import numpy as np
+from scipy.special import expit
+from sklearn.preprocessing import normalize
+
+__authors__ = ['Rafaelle Aygalenq','Sarah Lina Hammoutene','Dora Linda Kocsis','Noémie Quéré']
+__emails__  = ['b00724587@essec.edu','b00712035@essec.edu','b00714326@essec.edu','b00719656@essec.edu']
+
+def text2sentences(path):
+    # feel free to make a better tokenization/pre-processing
+    sentences = []
+    with open(path) as f:
+        for l in f:
+            sentences.append( l.lower().split() )
+    return sentences
+
+def loadPairs(path):
+    data = pd.read_csv(path,delimiter='\t')
+    pairs = zip(data['word1'],data['word2'],data['similarity'])
+    return pairs
+
+
+class mySkipGram:
+    def __init__(self,sentences, nEmbed=100, negativeRate=5, winSize = 5, minCount = 5):
+        raise NotImplementedError('implement it!')
+
+
+    def train(self,stepsize, epochs):
+        raise NotImplementedError('implement it!')
+
+    def save(self,path):
+        raise NotImplementedError('implement it!')
+
+    def similarity(self,word1,word2):
+        """
+            computes similiarity between the two words. unknown words are mapped to one common vector
+        :param word1:
+        :param word2:
+        :return: a float \in [0,1] indicating the similarity (the higher the more similar)
+        """
+        raise NotImplementedError('implement it!')
+
+    @staticmethod
+    def load(path):
+        raise NotImplementedError('implement it!')
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--text', help='path containing training data', required=True)
+    parser.add_argument('--model', help='path to store/read model (when training/testing)', required=True)
+    parser.add_argument('--test', help='enters test mode', action='store_true')
+
+    opts = parser.parse_args()
+
+    if not opts.test:
+        sentences = text2sentences(opts.text)
+        sg = mySkipGram.gSkipGram(sentences)
+        sg.train()
+        sg.save(opts.model)
+
+    else:
+        pairs = loadPairs(opts.text)
+
+        sg = mySkipGram.load(opts.model)
+        for a,b,_ in pairs:
+            print(sg.similarity(a,b))
